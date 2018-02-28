9c477fc2c6fc637c56c86055bb8b36eef8fe20fa
Rounder1 <tolerdavid@gmail.com>
2018-02-28 18:00:35 -0500

    bug fix

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,23 +15,27 @@ window.onload = function () {
     var randomWord = listOfWords[Math.floor(Math.random() * listOfWords.length)]; 
     var goalWord = randomWord.toUpperCase();
 
-    // Store the letters the player guesses in arrays so it can be more easily maipulated
+    // Store the letters the player guesses in arrays
     var playerStatus = []; 
     var playerWrongLetters = [];
+    var playeRightLetters = [];
 
-   
+   function newGame () {
+       
+   }
 
 
 
 
-    // This loop will assing the playerStatus Array an underscore for each letter in goalWord, it will also determine the number of correct guesses needed to win
+    // This loop will create an underscore in the playerStatus array for each letter in goalWord, it will also determine the number of correct guesses needed to win
     for (i=0; i < goalWord.length; i++) { 
         playerStatus[i] = "_";
         guessesToWin++;
     }
 
-    // This converts the playerStatus array into a string with the join method and adds a space to seperate each letter. Then writes the string to the document 
+    // This converts the playerStatus array into a string with the join method and adds a space to seperate each letter. Then writes the string to the document.
     document.getElementById("word_underscores").innerHTML = playerStatus.join(" ");
+    // These write the player's stats to the secreen when the page first loads
     document.getElementById("guesses_remaining").innerHTML = "Guesses Remaining: " + playerGuessesLeft;
     document.getElementById("wins_tag").innerHTML = "Wins: " + playerWins;
 
@@ -47,22 +51,24 @@ window.onload = function () {
         var pressedKey = event.key; 
         var guessedLetter = pressedKey.toUpperCase();
 
-        //checks to make sure the pressed key is a letter And that the user has not guessed it befor.
-        if (regexLettersOnly.test(guessedLetter) && playerWrongLetters.includes(guessedLetter) === false) { 
+        //checks to make sure the pressed key is a letter And that the user has not guessed it before.
+        if (regexLettersOnly.test(guessedLetter) && playerWrongLetters.includes(guessedLetter) === false && playeRightLetters.includes(guessedLetter) === false) { 
 
-            // checks to make sure the pressed key matches a letter(s) in the goalWord, if it does then it replaces the corilating underscore with that letter 
+            // checks to make sure the pressed key matches a letter(s) in the goalWord, if it does then it replaces the correlating underscore with that letter
+            // and makes sure the user can't guess that same letter. 
             for (i=0; i < goalWord.length; i++) { 
                 if (goalWord.charAt(i) === guessedLetter) {
                     playerStatus[i] = guessedLetter;
+                    playeRightLetters.push(guessedLetter);
                     playerGuessCorrect = true;
                     correctGuesses++;
                 } 
             }
 
-            // Updates the document with the new value of playerStatus
+            // Updates the document with the new value of playerStatus so that the user can see their correct guesses so far
             document.getElementById("word_underscores").innerHTML = playerStatus.join(" ");
 
-            // Keep track of player wrong guesses 
+            // Keeps track of both the player's wrong guesses and how many guesses they have left 
             if (playerGuessCorrect !== true) {
                 playerGuessesLeft = playerGuessesLeft -1;
                 playerWrongLetters.push(guessedLetter);
@@ -70,11 +76,12 @@ window.onload = function () {
 
             // Check to see if player wins, then reset the game
             if (correctGuesses === guessesToWin) {
+                playerWins++;
+
                 guessesToWin = 0;
                 playerGuessesLeft = 16;
                 correctGuesses = 0;
-                playerWrongLetters = [];
-                playerWins++;
+                playerWrongLetters = []; 
                 document.getElementById("guessed_letters").innerHTML = playerWrongLetters.join();
                 document.getElementById("win_or_lose").innerHTML = "You win! Congratulations! Press any key to play again.";
                 document.getElementById("guesses_remaining").innerHTML = "Guesses Remaining: ";
